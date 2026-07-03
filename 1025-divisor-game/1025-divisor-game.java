e9cd489fee454fcb2f2cb8af834e6d0a0fc9dead
Saksham16926 <104881545+Saksham16926@users.noreply.github.com>
2026-07-04 01:24:26 +0530

    Time: 0 ms (100%), Space: 42 MB (53.46%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        
+        if(n == 1) return false;
+        if(n % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+}
