ebb5b36d2272da7e20bf86019269bbe43ac440cd
Saksham16926 <104881545+Saksham16926@users.noreply.github.com>
2026-07-08 22:56:01 +0530

    Time: 2 ms (16.67%), Space: 42.4 MB (10.73%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,20 @@
+class Solution {
+    Map<Long,Integer> map = new HashMap<>();
+    public int integerReplacement(int n){
+        return (int) helper((long) n);
+    }
+
+    public long helper(long n){
+        if(n == 1) return 0;
+        if(map.containsKey(n)) return map.get(n);
+
+        long steps;
+        if( n % 2 == 0){
+            steps = 1 + helper(n/2);
+        }else{
+            steps = 1 + Math.min(helper(n + 1), helper(n - 1));
+        }
+        map.put(n , (int) steps);
+        return steps;
+    }
+}
