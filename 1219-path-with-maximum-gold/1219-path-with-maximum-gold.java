df85b6253486626c91a0ee35057346c583579c0e
Saksham16926 <104881545+Saksham16926@users.noreply.github.com>
2026-07-13 21:49:46 +0530

    Time: 62 ms (72.47%), Space: 43.1 MB (24.91%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int getMaximumGold(int[][] grid) {
+        int maxGold = 0;
+      
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != 0) {
+                    maxGold = Math.max(maxGold, dfs(grid, i, j));
+                }
+            }
+        }
+        
+        return maxGold;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+            return 0;
+        }
+
+        int currentGold = grid[i][j];
+        
+        grid[i][j] = 0;
+
+        int up = dfs(grid, i - 1, j);
+        int down = dfs(grid, i + 1, j);
+        int left = dfs(grid, i, j - 1);
+        int right = dfs(grid, i, j + 1);
+
+        int maxPath = Math.max(Math.max(up, down), Math.max(left, right));
+
+        grid[i][j] = currentGold;
+        return currentGold + maxPath;
+    }
+}
