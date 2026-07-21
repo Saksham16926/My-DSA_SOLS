90bbd3bbfd280034f779c3facb25c139acc14ad0
Saksham16926 <104881545+Saksham16926@users.noreply.github.com>
2026-07-21 23:23:46 +0530

    Time: 6 ms (72.61%), Space: 45.3 MB (34.55%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,30 @@
-class Solution { 
-    int count = 0;
+class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, target, 0, 0);
-    }
-
-    public int helper(int[] nums, int target, int index, int sum){
-        if(index == nums.length){
-            if(sum == target){
-                count ++;
-                return count;
-            }else{
-                return count;
-            }
+        int ts = 0;
+        for (int num : nums) {
+            ts += num;
+        }
+        if (ts < Math.abs(target) || (target + ts) % 2 != 0) {
+            return 0;
         }
 
-        int left = helper(nums, target, index + 1, sum + nums[index]);
-        int right = helper(nums, target, index + 1, sum - nums[index]);
+        int subtar = (target + ts) / 2;
+        
+        int[][] dp = new int[nums.length + 1][subtar + 1];
 
-        return count;
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = 0; j <= subtar; j++) {
+                
+                dp[i][j] = dp[i - 1][j];
+                
+                if (j >= nums[i - 1]) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
+                
+            }
+        }
+        return dp[nums.length][subtar];
     }
 }
