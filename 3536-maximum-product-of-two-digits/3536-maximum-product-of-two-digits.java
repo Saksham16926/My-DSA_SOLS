bfebe9c2eb1a1890fbb0c7f574648ab305705b2f
Saksham16926 <104881545+Saksham16926@users.noreply.github.com>
2026-07-26 02:17:55 +0530

    Time: 9 ms (2.41%), Space: 43.5 MB (5.73%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxProduct(int n) {
+        int[] arr = String.valueOf(n).chars().map(c -> c - '0').toArray();
+        Arrays.sort(arr);
+
+            return arr[arr.length - 1] * arr[arr.length - 2];
+    }
+}
