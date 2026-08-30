51dd7ea3cda41a51ad1bded36de64dfe2e636294
Saksham16926 <104881545+Saksham16926@users.noreply.github.com>
2026-08-30 23:07:54 +0530

    Time: 2 ms (85.47%), Space: 46.2 MB (88.95%) - LeetHub

---
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String tree2str(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        helper(root, sb);
+        return sb.toString();     
+    }
+
+    public void helper(TreeNode root, StringBuilder sb){
+        if(root == null){
+            return;
+        }
+        sb.append(root.val);
+        if (root.left == null && root.right == null) {
+            return;
+        }
+
+        sb.append("(");
+        if (root.left != null) {
+            helper(root.left, sb);
+        }
+        sb.append(")");
+        if (root.right != null) {
+            sb.append("(");
+            helper(root.right, sb);
+            sb.append(")");
+        }
+    }
+}
