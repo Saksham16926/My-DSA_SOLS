56f62cda9d5b43c1a80603a286ab3aebccc0eb50
Saksham16926 <104881545+Saksham16926@users.noreply.github.com>
2026-09-08 22:15:15 +0530

    Time: 38 ms (95.15%), Space: 99.3 MB (68.81%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes == null || envelopes.length == 0) return 0;
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            }
+            return a[0] - b[0];
+        });
+
+        int[] dp = new int[envelopes.length];
+        int len = 0;
+
+        for (int[] envelope : envelopes) {
+            int height = envelope[1];
+            
+            int index = Arrays.binarySearch(dp, 0, len, height);
+            
+            if (index < 0) {
+                index = -(index + 1);
+            }
+            
+            dp[index] = height;
+            
+            if (index == len) {
+                len++;
+            }
+        }
+        
+        return len;
+    }
+}
